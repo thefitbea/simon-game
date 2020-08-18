e51bec24333bcd407bf2b2313c8f68c2c34af3e4
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-08-18 12:23:03 +0530

    Add files via upload

--- javascript/game.js
+++ javascript/game.js
@@ -1,21 +1,73 @@
-var randomNumber,randomChosenColor;
-var gamePattern = [];
-var buttonColours = ["red","yellow","blue","green"];
+var randomNumber, randomChosenColor, level = 0,
+  started;
+var gamePattern = [],
+  userClickedPattern = [],
+  buttonColours = ["red", "yellow", "blue", "green"];
 
-var userClickedPattern = [];
 
-$(".btn").click(function(){
+  $(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
+
+$(".btn").click(function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
 });
 
 
+// ALTERNATE_STUPID_LOGICS
+// do{
+//   $("body").on("keypress", function(event){
+//     nextSequence();
+//   });
+//   doWhile++;
+// }
+// while(doWhile < 0);
 
-function nextSequence(){
+
+// $("body").on("keypress", function(event) {
+//   if (started===false) {
+//     nextSequence();
+//   }
+//   else{
+//     $("#level-title").text("Level "+ level);
+//   }
+//   $("body").toggle(started);
+// });
+
+
+
+
+
+
+
+function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  playSound(randomChosenColor);
+  level++;
+  $("#level-title").text("Level " + level);
+}
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed")
+  }, 100)
 }
